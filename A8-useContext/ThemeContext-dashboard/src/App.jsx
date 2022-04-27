34b6113c8f6c8c874bfc3bf8d463959f2a7ce6f0
Hashmat Noorani <hashmatwani@icloud.com>
2022-04-27 14:57:35 +0530

    done

---
+++ A8-useContext/ThemeContext-dashboard/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { LeftMenu } from "./components/LeftMenu";
+import { RightMenu } from "./components/RightMenu";
+
+function App() {
+  return (
+    <div className="App">
+      <LeftMenu />
+      <RightMenu />
+    </div>
+  );
+}
+
+export default App;
---
+++ A8-useContext/ThemeContext-dashboard/src/components/styled-components/Toggle.jsx
@@ -0,0 +1,8 @@
+import styledComponents from "styled-components";
+export const Toggle = styledComponents.div`position: absolute;
+left:${({ theme }) => (theme === "light" ? 0 : null)};
+right:${({ theme }) => (theme === "dark" ? 0 : null)};
+width: 47px;
+height: 47px;
+border-radius: 50%;
+background: #29cb97;`;
